58bbb81114a7c26c42bd19327b63cb386ffa7efa
Allis <dveasia558@gmail.com>
2022-03-22 00:02:44 +0800

    修改 navbar 導覽列

---
+++ Controllers/PartViewController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EXhibition.Controllers
+{
+    public class PartViewController : Controller
+    {
+        // GET: PartView
+        public ActionResult Navbar()
+        {
+            return PartialView();
+        }
+    }
+}
