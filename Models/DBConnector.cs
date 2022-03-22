78ff3d44dffdb9365c6afd77e6b777968a2ade6a
Allis <dveasia558@gmail.com>
2022-03-22 19:57:34 +0800

    建立新版 連線物件

---
+++ Models/DBConnector.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EXhibition.Models
+{
+    public class DBConnector : ExhibitionEntities
+    {
+        public DBConnector() : base()
+        {
+            base.Configuration.ProxyCreationEnabled = false;
+            Database.Connection.ConnectionString = Environment.GetEnvironmentVariable("SQL_CONNECTSTRING");
+        }
+    }
+}
