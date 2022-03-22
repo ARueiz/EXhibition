5eca1efabeb216c57e4689ea2db24bb386055b64
dveasia558 <dveasia558@gmail.com>
2022-03-22 15:32:04 +0800

    根據用戶權限給予對應的 navbar

--- Controllers/PartViewController.cs
+++ Controllers/PartViewController.cs
@@ -7,7 +7,11 @@ namespace EXhibition.Controllers
         // GET: PartView
         public ActionResult Navbar()
         {
-            return PartialView();
+            int x = 1;
+            if (x == 1)
+                return PartialView("_NavbarUser");
+            else
+                return PartialView("_NavbarVisitor");
         }
     }
 }
