848914d44f25b01ba5cb58e54870f31937555239
dveasia558 <dveasia558@gmail.com>
2022-03-22 10:41:04 +0800

    fix css js import error

--- App_Start/BundleConfig2.cs
+++ App_Start/BundleConfig2.cs
@@ -12,7 +12,7 @@ namespace EXhibition
                         "~/Scripts/JQuery/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                        "~/Scripts/JQuery/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
@@ -20,11 +20,11 @@ namespace EXhibition
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/Bootstrap/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Bootstrap/bootstrap.css",
+                      "~/Content/Bootstrap/site.css"));
         }
     }
 }
