489eaa47340626d8d6c95852028f680808e049ff
Allis <dveasia558@gmail.com>
2022-03-24 18:34:29 +0800

    將新增 api 並將 舊版 controller 移動至新 controller

--- Controllers/HostApiController.cs
+++ Controllers/HostApiController.cs
@@ -3,13 +3,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.Http.Cors;
 using System.Web.Mvc;
 using EXhibition.Models;
 
 namespace EXhibition.Controllers
 {
-    [Models.AllowCrossSiteJson]
     public class HostApiController : Controller
     {
 
--- Controllers/UserApiController.cs
+++ Controllers/UserApiController.cs
@@ -2,13 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.Http.Cors;
 using System.Web.Mvc;
 using EXhibition.Models;
 
 namespace EXhibition.Controllers
-{
-    [Models.AllowCrossSiteJson]
+{    
     public class UserApiController : Controller
     {
 
