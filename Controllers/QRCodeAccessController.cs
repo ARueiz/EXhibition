544c0991fca912e7e6e0caf8a0b6fa1d52a55f13
dv6230 <67547654+dv6230@users.noreply.github.com>
2022-03-27 00:30:18 +0800

    login api

---
+++ Controllers/QRCodeAccessController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EXhibition.Controllers
+{
+    public class QRCodeAccessController : ApiController
+    {
+        Models.DBConnector db = new Models.DBConnector();
+
+        public IHttpActionResult PostLogin([FromBody]Models.LoginToken token)
+        {
+            Dictionary<string, object> data = new Dictionary<string, object>();
+            data["token"] = token;
+            data["status"] = 200;
+            return Ok(data);
+        }
+
+        public IHttpActionResult GetLoginToken()
+        {
+            Models.LoginToken data = new Models.LoginToken();
+            var s = DateTime.Now.GetHashCode().ToString();
+            data.token = Guid.NewGuid().ToString() ;
+            //data.token = data.token +""+s;
+            db.QRCodeLoginToken.Add(new Models.QRCodeLoginToken() { token = data.token , createTime = DateTime.Now.AddHours(-1)});
+            db.SaveChanges();
+            //Dictionary<string, object> dict = new Dictionary<string, object>();
+            //dict["token"] = data.token;
+            //dict["DateTimeNowHash"] = s ;
+            //dict["TokenLength"] = data.token.Length;
+            return Ok(data) ;
+        }
+    }
+}
---
+++ Models/LoginToken.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EXhibition.Models
+{
+    public class LoginToken
+    {
+        public string token { get; set; }
+    }
+}
