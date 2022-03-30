7047bd5b63d75545f8168f00d7387af83a9b0d56
黃亭愷 <kuangloner@yahoo.com.tw>
2022-03-30 15:37:22 +0800

    Merge branch 'Azraekclash' into develop
    # Conflicts:
    #	Controllers/ExhibitorApiController.cs
    #	EXhibition.csproj

---
+++ Models/ApplyList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EXhibition.Models
+{
+    public class ApplyList
+    {
+        public int EVID { get; set; }
+        public int EID { get; set; }
+        public string name { get; set; }
+        public string startdate { get; set; }
+        public string enddate { get; set; }
+        public string venue { get; set; }
+        public string status { get; set; }
+        public bool dateout { get; set; }
+    }
+}
