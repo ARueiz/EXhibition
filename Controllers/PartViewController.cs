1fb97c1fd9a8944094f56be3f333322fd7dfea53
黃亭愷 <kuangloner@yahoo.com.tw>
2022-04-01 14:24:35 +0800

    no message

--- Controllers/PartViewController.cs
+++ Controllers/PartViewController.cs
@@ -24,10 +24,10 @@ namespace EXhibition.Controllers
         public ActionResult SideBarBtn()
         {
 
-            //if (HttpContext.IsDebuggingEnabled)
-            //{
-            //    Session["UserRole"] = "Host";
-            //}
+            if (HttpContext.IsDebuggingEnabled)
+            {
+                Session["UserRole"] = "Host";
+            }
 
             if (Session["UserRole"].ToString() == "User")
             {
@@ -45,7 +45,7 @@ namespace EXhibition.Controllers
 
         public ActionResult SideBar()
         {
-            //int authId = Session["auth"] == null ? 1 : (int)Session["auth"];
+            int authId = Session["auth"] == null ? 1 : (int)Session["auth"];
             if (Session["UserRole"].ToString() == "User")
             {
                 return PartialView("_SideBarUser");
