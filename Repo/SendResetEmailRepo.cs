6fab79e70e4a0cc563089e008061e5ffa9bffb02
dv6230 <67547654+dv6230@users.noreply.github.com>
2022-04-06 02:36:43 +0800

    no message

--- Repo/SendResetEmailRepo.cs
+++ Repo/SendResetEmailRepo.cs
@@ -11,18 +11,18 @@ namespace EXhibition.Repo
         static public void SendResetEmail(string email ,string uuid)
         {
             //MailMessage mail = new MailMessage();
-            //mail.From = new MailAddress("tt29334@gmail.com","tt29334");
+            //mail.From = new MailAddress("tt29334@gmail.com", "tt29334");
             //mail.To.Add("dveasia558@gmail.com");
             //mail.Priority = MailPriority.Normal;
             //mail.Subject = "密碼重設";
-            //mail.Body = "<a href='https://localhost:44378/home/ResetPassword/?uuid='"+ uuid +">重設</a>";
+            //mail.Body = "<a href='https://localhost:44378/home/ResetPassword/?uuid=" + uuid + "'>重設</a>";
             //mail.IsBodyHtml = true;
-            //SmtpClient smtp = new SmtpClient("smtp.gmail.com",587);
-            //smtp.Credentials = new System.Net.NetworkCredential("tt29334@gmail.com","");
+            //SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+            //smtp.Credentials = new System.Net.NetworkCredential("tt29334@gmail.com", "");
             //smtp.EnableSsl = true;
             //smtp.Send(mail);
             //smtp = null;
-            //smtp.Dispose();
+            //mail.Dispose();
         }
     }
 }
